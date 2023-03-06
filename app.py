b2796ea26108a85221f2d219fc2de6a3c4993b1e
joeylee <joey60504@gmail.com>
2023-03-06 17:59:57 +0800

    背景圖 省略重複部分 額外CSS 上至5.14

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask,render_template
 from product import product_list
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def home_page():
     return render_template('home.html',product_list = product_list)
 
-@app.route('/route_name')
-def product_show():
-    return render_template('product_show.html')
+@app.route('/product/<pid>')
+def product_show(pid):
+    pid = int(pid)
+    product = product_list[pid]
+    return render_template('product_show.html',product = product)
 
 if __name__ == '__main__':
     app.run(debug=True)
