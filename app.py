6d36007fa47bb3bf464f83dd053d0ffcef292535
joeylee <joey60504@gmail.com>
2023-03-09 17:55:31 +0800

    客製化內容邏輯

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from product import product_list
-from product import chair_customize_model_dic,chair_seat_customize_model_color_dic,chair_headandback_customize_model_color_dic
+from product import Backrest_Frame, headrest, Armrest, Seatpad_material, Mechaniam, Base, Casters
+from product import head_And_Backrest_Color, seat_Color, special_Color
 from product import table_electric_customize_model_dic, table_manual_customize_model_dic
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -20,7 +21,7 @@ def home_page():
         if 'fitting' in product['type']:
             fittingList.append(product)
         if 'music' in product['type']:
-            musicList.append(product)   
+            musicList.append(product)
     return render_template('home.html', chairList=chairList,
                            tableList=tableList, fittingList=fittingList, musicList=musicList)
 
@@ -28,30 +29,81 @@ def home_page():
 @app.route('/product/<pid>')
 def product_show(pid):
     pid = str(pid)
-    productList: dict
-    colorSeatList : dict
-    colorHeadAndBackList :dict
+    productList = {}
+    choiceDict = {}
+    colorDict = {}
     for list in product_list:
         title = list.get('title')
         if title == pid:
             productList = list
+    BackrestFrame = Backrest_Frame.copy()
+    Seatpadmaterial = Seatpad_material.copy()
+    headAndBackrestColor = head_And_Backrest_Color.copy()
+    seatColor = seat_Color.copy()
+    specialColor = special_Color.copy()
     # 椅子客製選項
     if 'chair_customize' == productList.get('type'):
-        choiceDict = chair_customize_model_dic
-        colorSeatList = chair_seat_customize_model_color_dic
-        colorHeadAndBackList = chair_headandback_customize_model_color_dic
+        #黑白框
+        if('Peacock' == productList.get('title')):
+            BackrestFrame.append('白')
+        #座墊標配等級Q綿->泡綿->網座    
+        if(productList.get('seat_Level') == 2):
+            Seatpadmaterial.remove('Q綿坐墊')
+        #背部可否升級網座    
+        #單框僅可選擇透氣網背
+        if(productList.get('back_SingleOrDouble') == False):
+            headAndBackrestColor.pop('HOA網座')
+            headAndBackrestColor.pop('泡綿布面')
+            headAndBackrestColor.pop('PVC合成皮')
+            headAndBackrestColor.pop('真牛皮')
+        #雙框部分款式可選擇透氣網背 其餘僅能網座/布面/皮    
+        if(productList.get('back_SingleOrDouble') == True):
+            if(productList.get('title') == 'Kangaroo' or
+               productList.get('title') == 'Bear' or
+               productList.get('title') == 'Peacock'):
+                headAndBackrestColor.pop('透氣網面')   
+        choiceDict["背框(Backrest Frame)"] = BackrestFrame
+        choiceDict["頭枕(Headrest)"] = headrest
+        choiceDict["扶手(Armrest)"] = Armrest
+        choiceDict["坐墊材質(Seatpad Material)"] = Seatpadmaterial
+        choiceDict["底盤功能(Mechaniam)"] = Mechaniam
+        choiceDict["椅腳(Base)"] = Base
+        choiceDict["椅輪(Casters)"] = Casters
+        colorDict["頭枕、椅背顏色(BackRest & HeadRest Color)"] = headAndBackrestColor
+        colorDict["坐墊顏色(Seatpad Color)"] = seatColor
+    # 椅子特規選項
+    if 'chair_special' == productList.get('type'):
+        if('Kabuto' == productList.get('title')):
+            BackrestFrame.append('白')
+        if(productList.get('seat_Level') == 4):
+            Seatpadmaterial.remove('Q綿坐墊')
+            Seatpadmaterial.remove('小網座')
+            Seatpadmaterial.remove('PVC合成皮')
+            Seatpadmaterial.remove('全牛皮')
+        if('Mamba' == productList.get('title')):
+            specialColor.remove('灰')
+        elif('Owl' == productList.get('title')):
+            specialColor.clear()
+            specialColor.add('頭枕背部銀灰色 | 椅座黑色')
+        choiceDict["背框(Backrest Frame)"] = BackrestFrame
+        choiceDict["坐墊材質(Seatpad Material)"] = Seatpadmaterial
+        colorDict["椅面顏色(Chair Color)"] = specialColor
+    # 椅子旗艦選項    
+    if 'chair_expensive' ==  productList.get('type'):
+        Seatpadmaterial.remove('Q綿坐墊')
+        Seatpadmaterial.remove('小網座')
+        Seatpadmaterial.remove('方厚成形泡綿')
+        headAndBackrestColor.pop('透氣網面')  
+        headAndBackrestColor.pop('泡綿布面')  
+        choiceDict["椅面材質(Chair Material)"] = Seatpadmaterial  
+        choiceDict["椅輪(Casters)"] = Casters
+        colorDict["椅面顏色(Chair Color)"] = headAndBackrestColor      
     # 桌子客製選項
     if 'table_electric_customize' == productList.get('type'):
         choiceDict = table_electric_customize_model_dic
-        colorSeatList = {}
-        colorHeadAndBackList = {}
     if 'table_manual_customize' == productList.get('type'):
         choiceDict = table_manual_customize_model_dic
-        colorSeatList = {}
-        colorHeadAndBackList = {}
-    return render_template('product_show.html', product=productList,
-                           choiceDict=choiceDict,colorSeatList = colorSeatList,
-                           colorHeadAndBackList = colorHeadAndBackList)
+    return render_template('product_show.html', product=productList, choiceDict=choiceDict ,colorDict = colorDict)
 
 
 if __name__ == '__main__':
--- product.py
+++ product.py
@@ -2,6 +2,7 @@
 #     椅子:
 #     客製款-->chair_customize
 #     特規款-->chair_special
+#     旗艦款-->chair_expensive
 #     桌子:
 #     電動款-->table_electric_customize
 #     手動款-->table_manual_customize
@@ -12,47 +13,114 @@
 #     wavebone:
 #     一般款:music_customize
 
+# 'image :"", 圖片位址 
+# 'title': '',產品名
+# 'type': '',型別
+# 'price': ,價格
+# 'is_published':,是否有貨
+# 'seat_Level':,坐墊等級 1=Q綿起 2=泡綿起 3=旗艦款 4=特規款
+# 'back_SingleOrDouble': 單框=False 雙框=True,
+    
+
 product_list = [{
     'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDM2LzE2MzUxNTQzMTBfMmJmYmJmMDAwZDVhNTE0YjNmMjIuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=7d4f36ec98355041",
     'title': 'Kangaroo',
     'type': 'chair_customize',
     'price': 7080,
-    'is_published': True
+    'is_published': True,
+    'seat_Level':2,
+    'back_SingleOrDouble':True,
+    
 },
     {
     'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDM5LzE2MjYzMTg5MjBfNTg1YjA5MTM2MWRhNWNlZmM5OTMuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=15eaea97635bf078",
     'title': 'Bear',
     'type': 'chair_customize',
     'price': 7080,
-    'is_published': True
+    'is_published': True,
+    'seat_Level':2,
+    'back_SingleOrDouble':True
+},
+    {
+    'image' :"https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI5NzkzNTM4L-e2sl8yN2Y5YmRhMGMxOGY0MWVjMjQxMC5qcGVnIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.jpeg?sha=c99764ebffbed854",
+    'title': 'KingKong',
+    'type': 'chair_expensive',
+    'price': 20400,
+    'is_published': True,
+    'seat_Level':3,
+    'back_SingleOrDouble':True
 },
     {
     'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzMzNjc2MDczLzE2MjE0MDM4MzVfYjQzYmQ2NTNhMzBhMWIxMTRkNGQuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=bc0f38bc9a5e18c8",
     'title': 'Kabuto',
     'type': 'chair_special',
     'price': 12880,
-    'is_published': True
+    'is_published': True,
+    'seat_Level':4,
+    'back_SingleOrDouble':True
 },
     {
     'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzM0MjYwMDA1LzE2Mjg4NDY4ODBfZTk5ODQwMWQ3NDE5OTAwMzc5MmUuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=6c3712c8fc3d756a",
     'title': 'Mamba',
     'type': 'chair_special',
     'price': 7880,
-    'is_published': True
+    'is_published': True,
+    'seat_Level':4,
+    'back_SingleOrDouble':True
 },
     {
-        'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDE5L0hZLTA2TV80ZjdiNjMyMzczMjQ1MzBmMmQ5Yi5qcGVnIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.jpeg?sha=23a2fe0bff2473ee",
+    'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDE5L0hZLTA2TV80ZjdiNjMyMzczMjQ1MzBmMmQ5Yi5qcGVnIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.jpeg?sha=23a2fe0bff2473ee",
     'title': 'Eagle',
     'type': 'chair_customize',
     'price': 4280,
-    'is_published': True
+    'is_published': True,
+    'seat_Level':1,
+    'back_SingleOrDouble':False
+},
+    {
+    'image':"https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzM4ODE4NDM1LzE2NzAyMzI5NDVfMDRmY2UyNjEzYzM5MGQ0OGZhODAucG5nIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.png?sha=f2a9d76f075b65d5",
+    'title': 'Owl',
+    'type': 'chair_special',
+    'price': 4600,
+    'is_published': True,
+    'seat_Level':4,
+    'back_SingleOrDouble':False
 },
     {
     'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDU0L-iqv-aVtOakheasvuWkp-Wwjy02MDB4NjAwXzYzMzM2OGFjMTNkNjMxYTlmYmJkLmpwZWciXSxbInAiLCJ0aHVtYiIsIjYwMHg2MDAiXV0.jpeg?sha=db736b12dbd55910",
     'title': 'Ox',
     'type': 'chair_customize',
     'price': 5800,
-    'is_published': True
+    'is_published': True,
+    'seat_Level':2,
+    'back_SingleOrDouble':True
+},
+    {
+    'image' :"https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDU3L2h5LTA2bV9kZGVlZmIwNmQzMDg2MmIxNWYxMi5qcGVnIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.jpeg?sha=80b62c67d84b0789",
+    'title': 'Hydra Lite',
+    'type': 'chair_customize',
+    'price' :4280,  
+    'is_published': True,
+    'seat_Level':1,
+    'back_SingleOrDouble':True
+},
+    {
+    'image':"https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI3MjAwMDk4L0hZLTAxTV9hNTU1NTk3NmQ3YTZhOWI3ZmMwNC5qcGVnIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.jpeg?sha=5b8483649b9bd3b8",
+    'title': 'Koala',
+    'type': 'chair_customize',
+    'price' :4380,  
+    'is_published': True,
+    'seat_Level':1,
+    'back_SingleOrDouble':True  
+},
+    {
+    'image' : "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzI2NDUxMDQyL0hZLTAxTV82YTdlYTUzOTQ4NzJiMTcyMmViOC5qcGVnIl0sWyJwIiwidGh1bWIiLCI2MDB4NjAwIl1d.jpeg?sha=b518a24764b00195",
+    'title': 'Deer',
+    'type': 'chair_customize',
+    'price' :4280,  
+    'is_published': True,
+    'seat_Level':1,
+    'back_SingleOrDouble':False
 },
     {
     'image': "https://cdn1.cybassets.com/media/W1siZiIsIjExNTM0L3Byb2R1Y3RzLzMxNDgzNDg4LzE2NTc1MTczNTNfZDI3NTFjNGFiYTdhYTljMDU1M2MuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=c90d0a01e904ec4e",
@@ -90,70 +158,85 @@ product_list = [{
     'is_published': True
 }
 ]
-# 旗艦款
-chair_expensive_customize_dic = {
-    '椅輪(Casters)': [
-        '固定椅腳', 'PP耐磨椅輪', 'PU靜音椅輪', 'PU電鍍空心輪'
+#桌款客製
+table_electric_customize_model_dic = {
+    '大小': [
+        '123cm*70cm', '150cm*75cm'
     ],
-}
-# 特規款
-chair_kabuto_customize_dic = {
-    '背框(Backrest Frame)': [
+    '桌板顏色': [
+        '黑', '白', '拼接木紋', '淺木紋'
+    ],
+    '桌腳顏色': [
         '黑', '白'
-    ],
-    '坐墊(Seatpad_material)': [
-        '泡綿', '強力網座'
-    ],
-    '網布顏色(Color)': [
-        '黑', '灰'
-    ],
+    ]
 }
-chair_viking_customize_dic = {
-    '坐墊(Seatpad_material)': [
-        '泡綿', '強力網座'
+table_manual_customize_model_dic = {
+    '大小': [
+        '120cm*60cm'
     ],
-    '網布顏色(Color)': [
-        '黑', '灰'
-    ],
+    '桌款顏色': [
+        '黑桌+黑腳', '白桌+白腳', '拼接木紋桌+黑腳', '淺木紋桌+白腳'
+    ]
 }
-chair_owl_customize_dic = {
-    '坐墊(Seatpad_material)': [
-        '泡綿', '強力網座'
-    ],
-    '網布顏色(Color)': [
-        '灰'
-    ],
+#椅款客製(special為特規款專用)
+Backrest_Frame = [
+    '黑'
+]
+headrest = [
+    '無頭枕','有頭枕'
+]
+Armrest = [
+    '無扶手', '1D', '旋轉1D', '3D', '3D肘拖', '旋轉3D扶手'
+]
+Seatpad_material = [
+    'Q綿坐墊', '方厚成形泡綿', '小網座', '強力網座', 'PVC合成皮', '全牛皮'
+]
+Mechaniam = [
+    'STG分離傾仰', 'XTG多段傾仰', 'SXTG滑動座殼'
+]
+Base = [
+    '尼龍椅腳', '鋁製椅腳'
+]
+Casters = [
+    '固定椅腳', 'PP耐磨椅輪', 'PU靜音椅輪'
+]
+head_And_Backrest_Color = {
+    'HOA網座': {
+        'HOA網座': [
+            'BMF-01文墨黑', 'BMF-02盎然綠', 'BMF-03晴空藍', 'BMF-04香檳金',
+            'BMF-06稻穗黃', 'BMF-07銀河灰', 'BMF-08尊貴黃', 'BMF-09烈焰紅', 'BMF-10葵花橘'
+        ]
+    },
+    '透氣網面': {
+        'HY彈性網布': [
+            'BK-01M炭晶黑', 'BK-02M韓花紅', 'BK-04M草原綠', 'BK-06M沉靜灰', 'BK-07M米月白', 'BK-08M秋波藍', 'BK-09M東方藍'
+        ]
+    },
+    '泡綿布面': {
+        '雨點布': [
+            '26-60雅典黑', '26-61暖灰', '26-63玉子黃', '26-64珊瑚灰', '26-65紅鬱金',
+            '26-66芥子綠', '26-67海水藍', '26-68瑪瑙紅', '26-69朽葉棕'
+        ],
+        '傢飾布': [
+            '37-102珍珠白', '37-102-1唐茶棕', '37-102-2銀鼠棕', '37-102-3青竹綠', '37-102-4赤緋紅', '37-102-5燕脂紅',
+            '37-102-6菖蒲紫', '37-102-7秋夜藍', '37-102-8銀月灰', '37-102-9玄鐵灰', '37-102-10琵琶棕',
+            '37-102-11博雅黑', '37-102-12藤黃', 'C03-1象牙白', 'C03-2白茶棕', 'C03-3水銀灰', 'C03-4胭脂紅',
+            'C03-5深緋紅', 'C03-6桔梗紫', 'C03-7群青藍', 'C03-8黑橡藍', 'C03-9桑茶黃', 'C03-10秋芒綠', 'C03-11深川藍',
+            'C03-12千歲綠', 'C03-13白橡灰', 'C03-14礪茶棕', 'C03-15焦茶棕', 'C03-16胡桃灰', 'C03-17古月灰', 'C03-18遂空黑'
+        ]
+    },
+    'PVC合成皮': {
+        'PVC合成皮': [
+            '黑'
+        ]
+    },
+    '真牛皮': {
+        '真牛皮': [
+            '黑'
+        ]
+    }
 }
-chair_mamba_customize_dic = {
-    '坐墊(Seatpad_material)': [
-        '泡綿', '強力網座'
-    ],
-    '網布顏色(Color)': [
-        '黑'
-    ],
-}
-# 一般客製款
-chair_customize_model_dic = {
-    '頭枕(Headrest)': [
-        '有頭枕', '無頭枕'
-    ],
-    '扶手(Armrest)': [
-        '無扶手', '1D', '旋轉1D', '3D', '3D肘拖', '旋轉3D扶手'
-    ],
-    '坐墊材質(Seatpad_material)': [
-        'Q綿坐墊', '方厚成形泡綿', '圓泡綿', '小網座', '強力網座'
-    ],
-    '底盤功能(Mechaniam)': [
-        'STG分離傾仰', 'XTG多段傾仰', 'SXTG滑動座殼'
-    ],
-    '椅腳(Base)': [
-        '尼龍椅腳', '鋁製椅腳'
-    ],
-    '椅輪(Casters)': [
-        '固定椅腳', 'PP耐磨椅輪', 'PU靜音椅輪', 'PU電鍍空心輪'
-    ],
-}
-chair_seat_customize_model_color_dic = {
+seat_Color = {
     'HOA網座': {
         'HOA網座': [
             'BMF-01文墨黑', 'BMF-02盎然綠', 'BMF-03晴空藍', 'BMF-04香檳金',
@@ -187,58 +270,6 @@ chair_seat_customize_model_color_dic = {
         ]
     }
 }
-chair_headandback_customize_model_color_dic = {
-    'HOA網座': {
-        'HOA網座': [
-            'BMF-01文墨黑', 'BMF-02盎然綠', 'BMF-03晴空藍', 'BMF-04香檳金',
-            'BMF-06稻穗黃', 'BMF-07銀河灰', 'BMF-08尊貴黃', 'BMF-09烈焰紅', 'BMF-10葵花橘'
-        ]
-    },
-    'HY彈性網布': {
-        'HY彈性網布': [
-            'BK-01M炭晶黑', 'BK-02M韓花紅', 'BK-04M草原綠', 'BK-06M沉靜灰', 'BK-07M米月白', 'BK-08M秋波藍', 'BK-09M東方藍'
-        ]
-    },
-    'HY彈性布': {
-        '雨點布': [
-            '26-60雅典黑', '26-61暖灰', '26-63玉子黃', '26-64珊瑚灰', '26-65紅鬱金',
-            '26-66芥子綠', '26-67海水藍', '26-68瑪瑙紅', '26-69朽葉棕'
-        ],
-        '傢飾布': [
-            '37-102珍珠白', '37-102-1唐茶棕', '37-102-2銀鼠棕', '37-102-3青竹綠', '37-102-4赤緋紅', '37-102-5燕脂紅',
-            '37-102-6菖蒲紫', '37-102-7秋夜藍', '37-102-8銀月灰', '37-102-9玄鐵灰', '37-102-10琵琶棕',
-            '37-102-11博雅黑', '37-102-12藤黃', 'C03-1象牙白', 'C03-2白茶棕', 'C03-3水銀灰', 'C03-4胭脂紅',
-            'C03-5深緋紅', 'C03-6桔梗紫', 'C03-7群青藍', 'C03-8黑橡藍', 'C03-9桑茶黃', 'C03-10秋芒綠', 'C03-11深川藍',
-            'C03-12千歲綠', 'C03-13白橡灰', 'C03-14礪茶棕', 'C03-15焦茶棕', 'C03-16胡桃灰', 'C03-17古月灰', 'C03-18遂空黑'
-        ]
-    },
-    'PVC合成皮': {
-        'PVC合成皮': [
-            '黑'
-        ]
-    },
-    '真牛皮': {
-        '真牛皮': [
-            '黑'
-        ]
-    }
-}
-table_electric_customize_model_dic = {
-    '大小': [
-        '123cm*70cm', '150cm*75cm'
-    ],
-    '桌板顏色': [
-        '黑', '白', '拼接木紋', '淺木紋'
-    ],
-    '桌腳顏色': [
-        '黑', '白'
-    ]
-}
-table_manual_customize_model_dic = {
-    '大小': [
-        '120cm*60cm'
-    ],
-    '桌款顏色': [
-        '黑桌+黑腳', '白桌+白腳', '拼接木紋桌+黑腳', '淺木紋桌+白腳'
-    ]
+special_Color = {
+    '黑','灰'
 }
